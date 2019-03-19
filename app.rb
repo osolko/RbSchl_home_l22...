844893716e7014555d20614b40e7e0ddd8b0ad80
osolko <%3T2v27Iwo>
2019-03-19 23:34:07 +0200

    no changes from preview just test git

--- app.rb
+++ app.rb
@@ -26,8 +26,8 @@ post '/visit' do
 	@username = params[:username]
 	@phonenum = params[:phone]
 	@datetime = params[:datetime]
-	@worker   = params[:barber]
-
+	@worker   = params[:barber] 
+	
 	f = File.open "public/users.txt", "a"  #а дописуємо в кінець файлу
  	f.write "Customer : #{@username} , #{@phonenum},  when: #{@datetime} \n\t worker: #{@worker} \n"
  	f.close		
