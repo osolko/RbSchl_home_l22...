9aef28ef0eba7f37f16efdfeb67c1858548a44d8
osolko <o.savka@gmail.com>
2019-03-28 14:54:06 +0200

    add colorpiker

--- app.rb
+++ app.rb
@@ -26,10 +26,11 @@ post '/visit' do
 	@username = params[:username]
 	@phonenum = params[:phone]
 	@datetime = params[:datetime]
-	@worker   = params[:barber] 
+	@worker   = params[:barber]
+	@color	  = params[:colorpicker] 
 	
 	f = File.open "public/users.txt", "a"  #а дописуємо в кінець файлу
- 	f.write "Customer : #{@username} , #{@phonenum},  when: #{@datetime} \n\t worker: #{@worker} \n"
+ 	f.write "Customer : #{@username} , #{@phonenum},  when: #{@datetime} \n\t worker: #{@worker} , hair color: #{@color} \n"
  	f.close		
 
  	erb "Thank you <b>#{@username.capitalize}</b>, we will contact with you ASAP"
